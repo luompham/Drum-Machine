6755c8f85113aa96f793a366d3515a2e5396e063
Luom Pham <luompham95@gmail.com>
2023-02-22 02:11:08 +0700

    passed challenge

--- index.js
+++ index.js
@@ -3,46 +3,86 @@ class App extends React.Component {
     constructor(props) {
         super(props)
 
+        this.state = {
+            audioId: '',
+            power: true,
+        }
+
         this.playAudio = this.playAudio.bind(this);
         this.handleKeyDown = this.handleKeyDown.bind(this);
-
-        this.handleKeyDown = this.handleKeyDown.bind(this)
+        this.handleKeyDown = this.handleKeyDown.bind(this);
+        this.handleClickPower = this.handleClickPower.bind(this);
     }
 
     playAudio(event) {
-        console.log(event.target.children[1]);
-        // console.log(event.target.querySelector('audio'));
-        const audioElement = event.target.children[1];
 
-        audioElement.play();
+        if (this.state.power) {
 
-    }
+            const audioElement = event.target.querySelector('audio');
+            audioElement.play();
 
-    handleKeyDown(event) {
+            this.setState({
+                audioId: event.target.id
+            })
 
+        } else {
 
-        let key = event.key.toUpperCase();
-
-        // let indexDiv = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-        const objDiv = document.querySelectorAll('.drum-pad')
-
-        for (let indexDiv = 0; indexDiv < 9; indexDiv++) {
-
-            let innerText = objDiv[indexDiv].children[0].innerText;
-            if (key === innerText) {
-                console.log('true')
-            } else {
-                console.log('false')
-
-            }
+            return;
         }
 
 
 
 
+    }
+
+    handleKeyDown(event) {
+
+        let key = event.key.toUpperCase();
+
+        const audioElement = document.querySelectorAll(`#${key}`)[0];
+
+        // console.log(document.getElementById(key).parentElement.id)
+
+        const audioId = document.getElementById(key).parentElement.id
+
+        if (audioElement) {
+
+            //  console.log(this.state.power);
+            if (this.state.power) {
+
+                this.setState({
+                    audioId: audioId
+                });
+
+                audioElement.play();
+
+            } else {
+
+                return;
+            }
+
+
+        } else {
+            //  console.log('not found audio element');
+            return;
+        }
+
+
 
     }
 
+
+
+    handleClickPower() {
+
+        this.setState({
+            power: !this.state.power,
+            audioId: ''
+        });
+
+        console.log(this.state.power)
+
+    }
 
 
     componentDidMount() {
@@ -68,39 +108,39 @@ class App extends React.Component {
 
                     <div className="pad-bank">
 
-                        <div onClick={this.playAudio} className="drum-pad" id="Heater 1"><span>Q</span>
+                        <div onClick={this.playAudio} className="drum-pad" id="Heater 1">Q
                             <audio className="clip" id="Q" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"></audio>
                         </div>
 
-                        <div className="drum-pad" id="Heater 2"><span>W</span>
+                        <div onClick={this.playAudio} className="drum-pad" id="Heater 2">W
                             <audio className="clip" id="W" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"></audio>
                         </div>
 
-                        <div className="drum-pad" id="Heater 3"><span>E</span>
+                        <div onClick={this.playAudio} className="drum-pad" id="Heater 3">E
                             <audio className="clip" id="E" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"></audio>
                         </div>
 
-                        <div className="drum-pad" id="Heater 4"><span>A</span>
+                        <div onClick={this.playAudio} className="drum-pad" id="Heater 4">A
                             <audio className="clip" id="A" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"></audio>
                         </div>
 
-                        <div className="drum-pad" id="Clap"><span>S</span>
+                        <div onClick={this.playAudio} className="drum-pad" id="Clap">S
                             <audio className="clip" id="S" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"></audio>
                         </div>
 
-                        <div className="drum-pad" id="Open-HH"><span>D</span>
+                        <div onClick={this.playAudio} className="drum-pad" id="Open-HH">D
                             <audio className="clip" id="D" src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"></audio>
                         </div>
 
-                        <div className="drum-pad" id="Kick-n'-Hat"><span>Z</span>
+                        <div onClick={this.playAudio} className="drum-pad" id="Kick-n'-Hat">Z
                             <audio className="clip" id="Z" src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"></audio>
                         </div>
 
-                        <div className="drum-pad" id="Kick"><span>X</span>
+                        <div onClick={this.playAudio} className="drum-pad" id="Kick">X
                             <audio className="clip" id="X" src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"></audio>
                         </div>
 
-                        <div className="drum-pad" id="Closed-HH"><span>C</span>
+                        <div onClick={this.playAudio} className="drum-pad" id="Closed-HH">C
                             <audio className="clip" id="C" src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"></audio>
                         </div>
 
@@ -113,9 +153,14 @@ class App extends React.Component {
 
                     <div className="control-wrapper">
 
-                        <div id="display">
-
+                        <div className="control_power">
+                            <div>Power</div>
+                            <div onClick={this.handleClickPower} className="select">
+                                <div style={{ 'float': this.state.power ? 'right' : 'left' }} className="select-on-off"></div>
+                            </div>
                         </div>
+
+                        <div className="control_display" id="display">{this.state.audioId}</div>
 
                     </div>
 
